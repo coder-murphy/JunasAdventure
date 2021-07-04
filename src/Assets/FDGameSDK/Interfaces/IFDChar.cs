27afd56ed407f53279a5fcb27728a9f3bf0af23f
oopsReach <oopsReach@foxmail.com>
2021-07-04 23:19:11 +0800

    接口添加

---
+++ src/Assets/FDGameSDK/Interfaces/IFDChar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.FDGameSDK.Interfaces
+{
+    /// <summary>
+    /// 角色接口
+    /// </summary>
+    public interface IFDChar : IFDObjectBase
+    {
+    }
+}
